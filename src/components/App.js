46dd46f363b225be620c2d76e1c131d054c013f0
Luke McInerney <mciluke@gmail.com>
2022-09-03 10:18:10 -0400

    search button

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
-import React, { Fragment } from 'react';
-import { render } from 'react-dom';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import SearchIcon from '@mui/icons-material/Search';
+import { DateRangePicker } from '@material-ui/pickers'
+
 
 function App() {
-    return (
-          <h1>Hello world</h1>
-    )
+  return <Button startIcon={<SearchIcon />}
+  onClick={() => console.log('clicked')} variant="contained">Search</Button>;
 }
+
 
 
 export default App;
